20f896a88c9d02f5d95d2b06b5827c0565ddfcdb
Vuong Thang <thangdv@blockchainlabs.asia>
2020-06-23 18:14:00 +0700

    tracking

--- app/feature/tracking-voting/tracking-voting.controller.js
+++ app/feature/tracking-voting/tracking-voting.controller.js
@@ -1,10 +1,20 @@
 const logger = require("app/lib/logger");
-const config = require("app/config");
 const StakingAPI = require("app/lib/staking-api")
 
 module.exports = async (req, res, next) => {
   try {
-    return res.ok();
+    let token = req.headers["x-access-token"] || req.headers["authorization"];
+    if (token && (token.startsWith("Bearer ") || token.startsWith("bearer "))) {
+      token = token.slice(7, token.length);
+    }
+    let result = await StakingAPI.voting(token, {
+      tx_id: req.body.tx_id,
+      voter_address: req.body.voter_address,
+      memo: req.body.memo,
+      type: req.body.type,
+      amount: req.body.amount
+    });
+    return res.status(result.httpCode).ok(result.data);
   }
   catch (err) {
     logger.error("tracking voting fail:", err);
